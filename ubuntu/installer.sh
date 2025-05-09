8b1cec24c156772f0c3c04cb7f661f414803e1e7
Fahad Kalil <fahadkalil@gmail.com>
2025-05-09 16:42:52 -0300

    Create installer.sh

---
+++ ubuntu/installer.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Script para Ubuntu Linux para instalacao de programas para Big Data
+clear
+
+echo ">>>> INSTALANDO SOFTWARES PARA AULAS DE BIG DATA"
+
+echo ">>>> Atualizando apt update"
+sudo apt install -y curl wget
+
+echo ">>>> Instalando Java OpenJDK..."
+sudo apt install openjdk-17-jdk -y &> /dev/null
+java -version
+
+echo ">>>> Instalando kcat e Apache Kafka (+ ZooKeeper)..."
+sudo apt install -y kcat
+
+
+curl https://dlcdn.apache.org/kafka/3.9.0/kafka_2.13-3.9.0.tgz --output kafka_2.13-3.9.0.tgz
+tar xzf kafka_2.13-3.9.0.tgz
+sudo mv kafka_2.13-3.9.0 /opt/kafka
+
+echo ">>>> Instalando Apache Flink..."
+curl https://dlcdn.apache.org/flink/flink-2.0.0/flink-2.0.0-bin-scala_2.12.tgz --output flink-2.0.0-bin-scala_2.12.tgz
+tar xzf flink-2.0.0-bin-scala_2.12.tgz
+sudo mv flink-2.0.0 /opt/flink
+
+echo ">>>> Instalando mongodb-community..."
+sudo apt install -y gnupg
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt update &> /dev/null
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl status mongod
+
+echo ">>>> Instalando client gui: MongoDB-Compass..."
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.45.4_amd64.deb
+sudo apt install -y ./mongodb-compass_1.45.4_amd64.deb
+
+echo ">>>> Instalando Apache Airflow ******"
+python -V
+
+echo "|  Criando e configurando virtualenv (venv): airflow-env"
+if [ ! -d "$HOME/airflow-env" ]; then
+	python -m venv airflow-env
+	source ~/airflow-env/bin/activate
+	pip install apache-airflow pandas
+	echo "|  Verificando instalação do Airflow e desativando venv"
+	airflow version
+	deactivate
+fi
+
+echo ">>>> Instalando plataforma Data Build Tool (dbt)..."
+if [ ! -d "$HOME/dbt-env" ]; then
+	echo "|  Criando e configurando virtualenv (venv): dbt-env"
+	python -m venv ~/dbt-env
+	source ~/dbt-env/bin/activate
+	pip install dbt-core dbt-postgres
+	echo "|  Verificando instalação do dbt e desativando venv"
+	dbt --version
+	deactivate
+fi
+
+echo ">>>> Instalando Apache Spark e pacote delta-spark..."
+if [ ! -d "$HOME/spark-env" ]; then
+	echo "|  Criando e configurando virtualenv (venv): spark-env"
+	python -m venv ~/spark-env
+	source ~/spark-env/bin/activate
+	pip install pyspark delta-spark
+	echo "|  Verificando instalação do PySpark e desativando venv"
+	pyspark --version
+	deactivate
+fi
+
+source ~/.bashrc &> /dev/null
+
+echo " "
+echo "#### INSTALAÇÕES FINALIZADAS! ####"

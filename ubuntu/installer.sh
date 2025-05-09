3c03c39eca26ec68525378c753047386d28272d3
Fahad Kalil <fahadkalil@gmail.com>
2025-05-09 18:38:52 -0300

    Update installer.sh
    Correção do PATH do Spark

--- ubuntu/installer.sh
+++ ubuntu/installer.sh
@@ -74,7 +74,7 @@ echo "|  Verificando instalação do Apache Spark"
 
 echo "|  Setando variáveis de ambiente"
 echo "export SPARK_HOME=/opt/spark" >> ~/.bashrc
-#echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc
+echo "export PATH=$SPARK_HOME/bin:$PATH" >> ~/.bashrc
 echo "export PYSPARK_PYTHON=$(which python)" >> ~/.bashrc
 source ~/.bashrc
 
